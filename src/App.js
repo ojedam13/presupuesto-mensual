876f590782d36cfe8b527bf88b519635fdcde31d
ojedam13 <m_ojeda17@hotmail.com>
2022-03-18 20:54:16 -0300

    mostrando gasto en interfaz

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import Pregunta from './components/Pregunta';
 import Formulario from './components/Formulario';
+import Listado from './components/Listado';
 
 
 function App() {
@@ -43,7 +44,9 @@ function App() {
               />
             </div>
             <div className='one-half column'>
-              2
+                  <Listado 
+                    gastos={gastos}
+              />
             </div>
           </div>
             )
